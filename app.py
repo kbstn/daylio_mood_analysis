b7ec6ca31a6f1b304ccf73893692ae2944a1d5cf
kbstn <konrad.bestian@umwelt.uni-giessen.de>
2022-11-11 18:37:45 +0100

    removed unneded libs

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ Created on Sun May 15 17:34:13 2022
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 from scipy.stats import zscore
-import altair as alt
 import plotly.express as px
-import matplotlib.pyplot as plt
 
 
 st.title('Upload a Daylio export .csv file')
